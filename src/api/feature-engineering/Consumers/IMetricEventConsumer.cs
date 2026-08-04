c35ce9d53fc9ca84a5e46459d8963d655c41bdc9
ravin00 <ravinbandara76@gmail.com>
2026-08-04 20:41:55 +0530

    feat: add IMetricEventConsumer interface as a marker for event consumer implementations

---
+++ src/api/feature-engineering/Consumers/IMetricEventConsumer.cs
@@ -0,0 +1,6 @@
+namespace AFIE.FeatureEngineering.Consumers;
+public interface IMetricEventConsumer
+{
+       // Marker interface. Implementations are also IHostedService, so DI wiring
+    // is done via AddHostedService<T> in Program.cs.
+}
