3b405f41673ebb87e005c251e8c0c8491b8f311b
ravin00 <ravinbandara76@gmail.com>
2026-08-04 12:59:10 +0530

    feat: add EventStalenessThresholdSeconds property to FeatureEngineeringOptions

--- src/api/feature-engineering/Models/FeatureEngineeringOptions.cs
+++ src/api/feature-engineering/Models/FeatureEngineeringOptions.cs
@@ -3,4 +3,5 @@ namespace AFIE.FeatureEngineering.Models;
 public class FeatureEngineeringOptions
 {
     public int WindowCapacity { get; set; } = 240;
+    public int EventStalenessThresholdSeconds { get; set; } = 60;
 }
