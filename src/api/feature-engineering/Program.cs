4e375e4fe62263c5949beba4367ebc4bba4b7179
ravin00 <ravinbandara76@gmail.com>
2026-08-04 20:41:47 +0530

    feat: refactor Program.cs to improve configuration and consumer mode handling

--- src/api/feature-engineering/Program.cs
+++ src/api/feature-engineering/Program.cs
@@ -1,3 +1,4 @@
+using AFIE.FeatureEngineering.Consumers;
 using AFIE.FeatureEngineering.Health;
 using AFIE.FeatureEngineering.Models;
 using AFIE.FeatureEngineering.Services;
@@ -5,12 +6,19 @@ using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<FeatureEngineeringOptions>(
-    builder.Configuration.GetSection("FeatureEngineering"));
+var section = builder.Configuration.GetSection("FeatureEngineering");
+builder.Services.Configure<FeatureEngineeringOptions>(section);
+builder.Services.Configure<EventHubOptions>(builder.Configuration.GetSection("EventHub"));
 
 builder.Services.AddSingleton<FeatureEngineeringHealthState>();
 builder.Services.AddSingleton<ActionHistoryStore>();
 builder.Services.AddSingleton<WindowStore>();
+
+var consumerMode = section["ConsumerMode"] ?? "local";
+if (consumerMode == "eventhub")
+    builder.Services.AddHostedService<EventHubConsumer>();
+else
+    builder.Services.AddHostedService<LocalJsonlTailConsumer>();
 
 builder.Services.AddHealthChecks()
     .AddCheck<FeatureEngineeringHealthCheck>("feature-engineering");
